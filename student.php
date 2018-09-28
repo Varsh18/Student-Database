36404c095d2411f4889d4f5a51286bbd32ffbd9d
varsh18 <varshiniravi1824@gmail.com>
2018-09-28 18:51:23 +0530

    Get option value to php

--- student.php
+++ student.php
@@ -4,6 +4,7 @@ if($_SERVER[REQUEST_METHOD]=='GET'){
 $name=$_GET['name'];
 $cgpa=$_GET['cgpa'];
 $id=$_GET['id'];
+$ch=$_GET['option'];
 $con=mysqli_connect("localhost","root","");
 if($con){
   $db=mysqli_select_db($con,"myDB");
