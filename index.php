c47bb5d5d67a6c473961c30087135ee597f678be
varsh18 <varshiniravi1824@gmail.com>
2018-09-29 19:44:33 +0530

    Updated content

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Student Database</title>
+    <link rel="stylesheet" href="css/styles.css" media="screen">
+    <link rel="icon" href="https://studentwellbeinghub.edu.au/public/img/element-02-blue.png" type="image/png"sizes="16">
+  </head>
+  <body>
+  <h1>STUDENT DATABASE</h1>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="get">
+  <div id="container">
+  <pre>
+  Name   :<input type="text" name="n" size="45"/><br/>
+  Id     :<input type="text" name="i" size="45"/></br>
+  CGPA   :<input type="text" name="c" size="45"/><br/>
+</pre>
+  <input type="radio" name="o" value="insert">Insert
+  <input type="radio" name="o" value="delete">Delete
+  <input type="radio" name="o" value="update">Update
+  <input type="radio" name="o" value="display">Display
+  <pre>
+
+    <input type="button" value="Submit"/>
+  </pre>
+</div>
+</form>
+</body>
+</html>
+<?php
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+if ($_SERVER["REQUEST_METHOD"] == "GET"){
+$name=test_input($_REQUEST['n']);
+$cgpa=test_input($_GET["c"]);
+$id=test_input($_GET["i"]);
+$ch=test_input($_GET["o"]);
+$con=mysqli_connect("localhost","root","");
+$db=mysqli_select_db($con,"mydb");
+if($con){
+  switch($ch){
+    case 'insert':{
+      if(isset($name) && isset($id) && isset($cgpa))
+      {
+      $query="INSERT INTO stud values('$name','$id','$cgpa')";
+      if(mysqli_query($con,$query))
+      echo "Inserted successfully";
+      else
+      echo "Error in insertion";
+      }
+      else
+      echo "Enter the required field";
+      break;
+    }
+    case 'delete':{
+      if(isset($id))
+      {
+      $query="delete from stud where id='$id'";
+      if(mysqli_query($con,$query))
+      echo "Deleted successfully";
+      else
+      echo "Error in Deletion";
+      }
+      else
+      echo "Enter the id of the student";
+    break;
+    }
+    case 'update':{
+      if(isset($id)&&isset($cgpa))
+      {
+      $query="update stud set cgpa='$cgpa' where id='$id'";
+      if(mysqli_query($con,$query))
+      echo "Updated successfully";
+      else
+      echo "Error in Updation";
+      }
+      else
+      echo "Enter the required field";
+      break;
+    }
+    case 'display':{
+      $query="select * from stud";
+      echo "<div id='display'>";
+      echo "<table><tr><th>Name</th><th>Id</th><th>Cgpa</th></tr>";
+      $res=mysqli_query($con,$query);
+      while($row=mysqli_fetch_array($res))
+        echo "<tr><td>$row[0]</td><td> $row[1]</td><td> $row[2]<td></tr><br/>";
+      echo "</table></div>";
+      break;
+    }
+  }
+}
+}
+?>
